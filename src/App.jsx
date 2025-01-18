9e1c7dad3e2ceb2be4752cff77332c35df3d06a9
홍재훈 <ehswkfn123@naver.com>
2025-01-18 15:45:41 +0900

    mobile header static

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div>
-      <h1 className='text-xl text-yellow-500 font-bold'>Hello Tailwind!</h1>
-      <p className='font-bold'>test</p>
+    <div className='bg-white-20 max-w-screen-2xl mx-auto'>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import LightMode from "../assets/icon/light.svg";
+
+function Header() {
+  return (
+    <div className='h-14 flex items-center justify-between relative px-6'>
+      <div className='absolute left-1/2 translate-x--1/2'>Logo</div>
+      <img src={LightMode} className='w-6 h-6 ml-auto' />
+    </div>
+  );
+}
+
+export default Header;
