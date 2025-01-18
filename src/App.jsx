1acf2bf9ca09a75026e5e55b97baa87a582d6e8a
홍재훈 <ehswkfn123@naver.com>
2025-01-18 11:55:21 +0900

    style : css 변수 설정 및 아이콘 다운

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 function App() {
   return (
     <div>
-      <h1 className='text-xl text-yellow-500 font-bold	'>Hello Tailwind!</h1>
+      <h1 className='text-xl text-yellow-500 font-bold'>Hello Tailwind!</h1>
+      <p className='font-bold'>test</p>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,31 @@ export default {
     fontFamily: {
       sans: "Pretendard , monospace",
     },
-    extend: {},
+    extend: {
+      colors: {
+        black: "#1F1F1F",
+        pink: "#FFCFD2",
+        red: "#FFADAD",
+        orange: "#FFD6A5",
+        yellow: "#FBF8CC",
+        green: "#CAFFBF",
+        "blue-10": "#98F5E1",
+        "blue-20": "#9BF6FF",
+        "purple-10": "#BDB2FF",
+        "purple-20": "#FFC6FF",
+        "white-10": "#FFFFFF",
+        "white-20": "#FFF8E7",
+        gray: "#C0C0C0",
+      },
+      fontWeight: {
+        regular: "400",
+        bold: "700",
+      },
+      screens: {
+        mobile: "393px",
+        destop: "1440px",
+      },
+    },
   },
   plugins: [],
 };
