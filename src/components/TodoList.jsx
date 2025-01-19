ff646e8681b35cc9ac93a4d4e0b6d129e0432d53
홍재훈 <ehswkfn123@naver.com>
2025-01-19 17:58:59 +0900

    destop todolist static

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,7 +2,7 @@ import TodoListItem from "./TodoListItem";
 
 function TodoList() {
   return (
-    <ul className='pl-3 pr-4 py-3 flex flex-col gap-2 grow overflow-auto'>
+    <ul className='pl-3 pr-4 py-7 flex flex-col gap-2 grow overflow-auto desktop:border-t-[1px] desktop:pl-6 desktop:gap-7'>
       <TodoListItem />
       <TodoListItem />
       <TodoListItem />
