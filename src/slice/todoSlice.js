85f18e28491e6de915efc557838b612e54a8af2d
홍재훈 <ehswkfn123@naver.com>
2025-01-23 20:19:04 +0900

    refactor : todo list item에 id 추가

--- src/slice/todoSlice.js
+++ src/slice/todoSlice.js
@@ -1,9 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = [
-  { title: "밥먹기", checked: false, date: "2024/01/01", hashtags: [] },
-  { title: "씻기", checked: true, date: "2024/02/01", hashtags: [] },
-  { title: "공부하기", checked: false, date: "2024/03/01", hashtags: [] },
+  { title: "밥먹기", checked: false, date: "2024/01/01", hashtags: [], id: 1 },
+  { title: "씻기", checked: true, date: "2024/02/01", hashtags: [], id: 2 },
+  {
+    title: "공부하기",
+    checked: false,
+    date: "2024/03/01",
+    hashtags: [],
+    id: 3,
+  },
 ];
 
 const todoSlice = createSlice({
@@ -15,6 +21,8 @@ const todoSlice = createSlice({
         title: action.payload,
         checked: false,
         date: "2024/01/01",
+        hasgtags: [],
+        id: Date.now(),
       });
     },
     checkList: (state, action) => {
