8ccf1ab20865023873e201cf8465a9b6458d046b
홍재훈 <ehswkfn123@naver.com>
2025-03-08 11:55:17 +0900

    featrue : 리스트 삭제 팝업 띄울때 현재 선택한 일정 제목 표시

--- src/components/TodoListDeletePopup.jsx
+++ src/components/TodoListDeletePopup.jsx
@@ -1,7 +1,9 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { returnDefault } from "../slice/modeSlice";
 import { deleteList } from "../slice/todoSlice";
 function TodoListDeletePopup({ selectedId }) {
+  const todoList = useSelector((state) => state.todoSlice.todoList);
+  const findList = todoList.find((todo) => todo.id === selectedId);
   const dispatch = useDispatch();
 
   function onClickConfirmedDeleteList() {
@@ -11,8 +13,8 @@ function TodoListDeletePopup({ selectedId }) {
   return (
     <div className='absolute top-0 w-full h-full bg-black bg-opacity-50 z-10'>
       <div className='w-[340px] h-[200px] bg-yellow rounded-xl py-4 text-center flex flex-col mx-auto mt-[280px] desktop:w-[500px] desktop:h-[280px] desktop:mt-[200px] desktop:py-8'>
-        <p className='text-xl font-bold mb-12 desktop:text-[48px] desktop:mb-[65px]'>
-          일정 제목
+        <p className='text-xl font-bold mb-10 desktop:text-[48px] desktop:mb-[65px]'>
+          {findList.title}
         </p>
         <p className='text-md grow desktop:text-[24px]'>
           일정을 삭제 하겠습니까?
