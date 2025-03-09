46d5061f62076965dfff58dd7c45b14b6ba7addf
홍재훈 <ehswkfn123@naver.com>
2025-03-09 18:27:54 +0900

    ui : 리스트 제목 클릭시 상세 페이지 이동 및 hover 효과 지정

--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -24,58 +24,61 @@ function TodoListItem({ todo, setSelectedId }) {
   }
 
   function onClickListTitle() {
+    setSelectedId(id);
     dispatch(modifyMode("listDetail"));
   }
 
   return (
-    <div onClick={() => setSelectedId(id)} className='cursor-pointer w-full'>
-      <li
-        className='bg-orange dark:bg-black h-[58px] rounded-2xl px-3 flex items-center 
+    <li
+      className='bg-orange dark:bg-black h-[58px] rounded-2xl px-3 flex items-center 
                  desktop:h-[120px] desktop:px-[34px] hover:bg-orange2 
                  w-full min-w-0'
+    >
+      <button onClick={() => checkTodoList()}>
+        <img
+          src={checked ? CheckedIcon : NotCheckedIcon}
+          className='mr-4 desktop:h-[58px] desktop:w-[58px] desktop:mr-6'
+        />
+      </button>
+      <div
+        className={`relative font-bold text-xl grow desktop:text-[48px] ${
+          checked && "line-through"
+        }`}
       >
-        <button onClick={() => checkTodoList()}>
-          <img
-            src={checked ? CheckedIcon : NotCheckedIcon}
-            className='mr-4 desktop:h-[58px] desktop:w-[58px] desktop:mr-6'
-          />
-        </button>
-        <div
-          className={`relative font-bold text-xl grow desktop:text-[48px] ${
-            checked && "line-through"
-          }`}
+        <span
+          className='dark:text-white hover:underline cursor-pointer'
           onClick={onClickListTitle}
         >
-          <p className='dark:text-white'>{title}</p>
-          <div className='absolute top-6 flex gap-2'>
-            {hashtags.map((tag, index) => (
-              <TodoListHashtag key={index} tag={tag} />
-            ))}
-          </div>
+          {title}
+        </span>
+        <div className='absolute top-6 flex gap-2'>
+          {hashtags.map((tag, index) => (
+            <TodoListHashtag key={index} tag={tag} />
+          ))}
         </div>
+      </div>
 
-        <div className='flex gap-3 desktop:gap-6'>
-          <button
-            className='desktop:w-[132px] desktop:h-[72px] desktop:bg-green desktop:rounded-xl hover:bg-green2'
-            onClick={onClickEditButton}
-          >
-            <img
-              src={editIcon}
-              className='desktop:mx-auto desktop:w-[50px] desktop:h-[50px]'
-            />
-          </button>
-          <button
-            className='desktop:w-[132px] desktop:h-[72px] desktop:bg-red desktop:rounded-xl hover:bg-red2'
-            onClick={onClickDeleteButton}
-          >
-            <img
-              src={deleteIcon}
-              className='desktop:mx-auto desktop:w-[50px] desktop:h-[50px]'
-            />
-          </button>
-        </div>
-      </li>
-    </div>
+      <div className='flex gap-3 desktop:gap-6'>
+        <button
+          className='desktop:w-[132px] desktop:h-[72px] desktop:bg-green desktop:rounded-xl hover:bg-green2'
+          onClick={onClickEditButton}
+        >
+          <img
+            src={editIcon}
+            className='desktop:mx-auto desktop:w-[50px] desktop:h-[50px]'
+          />
+        </button>
+        <button
+          className='desktop:w-[132px] desktop:h-[72px] desktop:bg-red desktop:rounded-xl hover:bg-red2'
+          onClick={onClickDeleteButton}
+        >
+          <img
+            src={deleteIcon}
+            className='desktop:mx-auto desktop:w-[50px] desktop:h-[50px]'
+          />
+        </button>
+      </div>
+    </li>
   );
 }
 
